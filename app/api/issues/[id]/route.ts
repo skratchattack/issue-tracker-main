80aedb84a0b969de408850baba9d23e8c30b4641
Orri <orrmann@gmail.com>
2023-10-23 19:12:00 +0300

    Assign an issue to a user

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -6,8 +6,8 @@ import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(request: NextRequest, { params }: { params: { id: string } }) {
-  const session = await getServerSession(authOptions);
-  if (!session) return NextResponse.json({}, { status: 401 });
+  // const session = await getServerSession(authOptions);
+  // if (!session) return NextResponse.json({}, { status: 401 });
 
   const body = await request.json();
   const validation = patchIssueSchema.safeParse(body);
--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,31 +1,41 @@
 "use client";
 
 import { Select } from "@radix-ui/themes";
-import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { User } from ".prisma/client";
+import { User, Issue } from ".prisma/client";
 import { useQuery } from "@tanstack/react-query";
-import { Skeleton } from '@/app/components'
+import { Skeleton } from "@/app/components";
 
-const AssigneeSelect = () => {
-    const {data: users, error, isLoading} = useQuery<User[]>({
-        queryKey: ['users'],
-        queryFn: () => axios.get('/api/users').then(res => res.data),
-        staleTime: 60 * 1000,
-        retry: 3
-    })
+const AssigneeSelect = ({ issue }: { issue: Issue }) => {
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 60 * 1000,
+    retry: 3,
+  });
 
-    if (isLoading) return <Skeleton />
+  if (isLoading) return <Skeleton />;
 
-    if (error) return null
+  if (error) return null;
 
+  const unassigned= ""
 
   return (
-    <Select.Root>
+    <Select.Root
+      defaultValue={issue.assignedToUserId || "unassigned"}
+      onValueChange={(userId) => { console.log(userId),
+        axios.patch(`/api/issues/${issue.id}`, { assignedToUserId: userId === "unassigned" ? null : userId });
+      }}
+    >
       <Select.Trigger placeholder="Assign..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
+          <Select.Item value="unassigned">Unassigned</Select.Item>
           {users?.map((user) => (
             <Select.Item key={user.id} value={user.id}>
               {user.name}
