d22c2234d37e64f35559a278ac10c326f23cf30d
Orri <orrmann@gmail.com>
2023-10-24 08:02:28 +0300

    Fix Filtering Bugs

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -1,12 +1,13 @@
-'use client'
+"use client";
 
 import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import React from "react";
 
 const IssueStatusFilter = () => {
-    const router = useRouter()
+  const router = useRouter();
+  const searchParams = useSearchParams();
 
   const statutes: { label: string; value?: Status }[] = [
     { label: "All", value: "ALL" },
@@ -15,14 +16,23 @@ const IssueStatusFilter = () => {
     { label: "Closed", value: "CLOSED" },
   ];
   return (
-    <Select.Root onValueChange={(status) => {
-        const query = status ? `?status=${status}` : 'ALL'
-        router.push('/issues/list' + query)
-    }}>
+    <Select.Root
+    defaultValue={searchParams.get('status') || 'ALL'}
+      onValueChange={(status) => {
+        const params = new URLSearchParams();
+        if (status) params.append("status", status);
+        if (searchParams.get("orderBy")) params.append("orderBy", searchParams.get("orderBy")!);
+        searchParams.get("orderBy");
+        const query = params.size ? "?" + params.toString() : "ALL";
+        router.push("/issues/list" + query);
+      }}
+    >
       <Select.Trigger placeholder="Filter by status..." />
       <Select.Content>
-        {statutes.map(status => (
-            <Select.Item key={status.value} value={status.value || "ALL"}>{status.label}</Select.Item>
+        {statutes.map((status) => (
+          <Select.Item key={status.value} value={status.value || "ALL"}>
+            {status.label}
+          </Select.Item>
         ))}
       </Select.Content>
     </Select.Root>
